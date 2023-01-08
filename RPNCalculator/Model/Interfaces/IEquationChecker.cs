2518feab9f274ffbe3d950dc8fbfcd76a5609b2b
alexGitSorokin <121434357+alexGitSorokin@users.noreply.github.com>
2023-01-08 13:47:27 +0300

    Update IEquationChecker.cs

--- RPNCalculator/Model/Interfaces/IEquationChecker.cs
+++ RPNCalculator/Model/Interfaces/IEquationChecker.cs
@@ -2,11 +2,12 @@
 {
     internal interface IEquationChecker
     {
+        #region Methods
         /// <summary>
         /// Проверка уравнение на корректность
         /// </summary>
         /// <param name="equation"></param>
         public bool Check(string equation);
-
+        #endregion
     }
 }
